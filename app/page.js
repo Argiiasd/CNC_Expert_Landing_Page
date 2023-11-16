e4367fbdee5f25712d4c89bf8d135a7d477b1ec5
Argiiasd <mateo.p.giacosa@gmail.com>
2023-11-16 05:15:20 -0300

    prueba lazy load

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
-import Presentation from "./ui/presentation.jsx/presentation";
-import About from "./ui/about/about";
-import Students from "./ui/students/students";
-import Footer from "./ui/footer/footer";
+// import Presentation from "./ui/presentation.jsx/presentation";
+// import About from "./ui/about/about";
+// import Students from "./ui/students/students";
+// import Footer from "./ui/footer/footer";
+import dynamic from "next/dynamic";
+const Presentation = dynamic(() => import("./ui/presentation.jsx/presentation"));
+const About = dynamic(() => import("./ui/about/about"));
+const Students = dynamic(() => import("./ui/students/students"));
+const Footer = dynamic(() => import("./ui/footer/footer"));
 
 export default function Home() {
   return (
