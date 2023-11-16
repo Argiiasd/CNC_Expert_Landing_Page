7fd7643c02a22e807cf461d98449c7e04483bd11
Argiiasd <mateo.p.giacosa@gmail.com>
2023-11-16 04:55:12 -0300

    footer

--- app/ui/footer/footer.jsx
+++ app/ui/footer/footer.jsx
@@ -5,15 +5,20 @@ export default function Footer() {
     return (
         <div className={styles.footerContainer}>
             <div className={styles.generalContainer}>
+                <div className={styles.content3}>
+                    <p className={styles.title}>Contáctanos:</p>
+                    <p className={styles.email}>aymara12vargas@gmail.com</p>
+                    <p className={styles.email}>karinespinola2004@gmail.com</p>
+                    <p className={styles.email}>nicolastedesco05@gmail.com</p>
+                    <p className={styles.email}>juandeangelis2004@gmail.com</p>    
+                </div>
+                <div className={styles.content2}>
+                    <p className={styles.text}>Nuestro email:</p>
+                    <p className={styles.text}>cnc.tecnica1.23@gmail.com</p>
+                </div>
                 <div className={styles.content1}>
                     <p className={styles.text}>CNC EXPERT © 2023</p>
                     <p className={styles.text}>Todos los derechos reservados.</p>
-                </div>
-                <div className={styles.content2}>
-                    <div>
-                        <p className={styles.text}>Nuestro email:</p>
-                        <p className={styles.text}>kLqzF@example.com</p>
-                    </div>
                 </div>
             </div>
         </div>
